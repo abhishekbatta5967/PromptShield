7f59d2531de57b81840744f54ec0de6ab56a7f39
abhishekbatta5967 <abhishekbatta5967@gmail.com>
2026-05-13 12:40:15 +0530

    post deployement commit 1

--- app.py
+++ app.py
@@ -97,182 +97,201 @@ if selected == "Prompt Analyzer":
 
     if st.button("Analyze Prompt"):
 
-        if user_prompt.strip() == "":
-            st.warning("Please enter a prompt.")
+        try:
 
-        elif len(user_prompt.split()) < 5:
-            st.info("Please enter a more detailed prompt for analysis.")
-            st.stop()
+            if user_prompt.strip() == "":
+                st.warning("Please enter a prompt.")
 
-        else:
-            progress = st.progress(0)
+            elif len(user_prompt.split()) < 5:
+                st.info("Please enter a more detailed prompt for analysis.")
+                st.stop()
 
-            with st.spinner("Analyzing prompt..."):
-                progress.progress(25)
-
-                result = hybrid_threat_analysis(user_prompt)
-                progress.progress(60)
-
-                save_attack_log(user_prompt, result)
-                progress.progress(100)
-
-            st.markdown("### Analysis Result")
-
-            if not result["threat_detected"]:
-                st.success("✅ Prompt Appears Safe")
-
-                st.markdown("---")
-
-                with st.spinner("Analyzing prompt efficiency..."):
-
-                    optimization_result = cached_prompt_optimization(
-                        user_prompt
-                    )
-
-                efficiency_score = optimization_result[
-                    "efficiency_score"
-                ]
-
-                if efficiency_score >= 85:
-                    grade = "A"
-
-                elif efficiency_score >= 70:
-                    grade = "B"
-
-                elif efficiency_score >= 50:
-                    grade = "C"
-
-                else:
-                    grade = "Needs Improvement"
-
-                # Donut Chart
-                chart = create_efficiency_chart(
-                    efficiency_score
-                )
-
-                st.plotly_chart(
-                    chart,
-                    use_container_width=True
-                )
-
-                st.metric(
-                    "Prompt Grade",
-                    grade
-                )
-
-                # Metrics
-                col1, col2, col3 = st.columns(3)
-
-                with col1:
-                    st.metric(
-                        "Clarity",
-                        optimization_result["clarity"]
-                    )
-
-                with col2:
-                    st.metric(
-                        "Specificity",
-                        optimization_result["specificity"]
-                    )
-
-                with col3:
-                    st.metric(
-                        "Structure",
-                        optimization_result["structure"]
-                    )
-
-                st.markdown("---")
-
-                # Suggestions
-                st.subheader("Prompt Improvement Suggestions")
-
-                for suggestion in optimization_result["suggestions"]:
-
-                    st.info(suggestion)
-
-                st.markdown("---")
-
-                # Optimized Prompt
-                st.subheader("Optimized Prompt")
-
-                st.success(
-                    optimization_result["optimized_prompt"]
-                )
-            
             else:
-                st.error("⚠️ Threat Detected")
+                progress = st.progress(0)
 
-                col1, col2 = st.columns(2)
+                with st.spinner("Analyzing prompt..."):
+                    progress.progress(25)
 
-                with col1:
-                    st.metric("Risk Score", result["final_risk_score"], delta=None)
+                    result = hybrid_threat_analysis(user_prompt)
+                    progress.progress(60)
 
-                with col2:
-                    st.metric("Severity", result["final_severity"], delta=None)
-            
+                    save_attack_log(user_prompt, result)
+                    progress.progress(100)
 
-                st.markdown("---")
+                st.markdown("### Analysis Result")
 
-                st.subheader("Detected Threat Types")
+                if not result["threat_detected"]:
+                    st.success("✅ Prompt Appears Safe")
 
-                st.warning(result["llm_threat_type"])
-                        
-                st.subheader("Detected Threat Categories")
+                    st.markdown("---")
 
-                if result["detected_threats"]:
-                    for threat in result["detected_threats"]:
-                        st.error(threat)
+                    with st.spinner("Analyzing prompt efficiency..."):
 
-                else:
-                    st.info("No rule-based threats detected.")
+                        optimization_result = cached_prompt_optimization(
+                            user_prompt
+                        )
 
-                st.markdown("---")
-
-                if result["llm_threat_type"] == "Gemini Overloaded":
-
-                    st.warning(
-                        "⚠️ Gemini API is currently overloaded. "
-                        "Using fallback security analysis."
+                    efficiency_score = optimization_result.get(
+                        "efficiency_score",
+                        50
                     )
 
-                st.subheader("Threat Explanation")
+                    if efficiency_score is None:
+                        efficiency_score = 50
 
-                st.write(result["explanation"])
+                    try:
+                        efficiency_score = int(
+                            efficiency_score
+                        )
 
-                st.markdown("---")
+                    except:
+                        efficiency_score = 50
 
-                st.subheader("Safe Rewrite Suggestion")
-                st.info(result["safe_rewrite"])
+                    if efficiency_score >= 85:
+                        grade = "A"
 
-                st.markdown("---")
+                    elif efficiency_score >= 70:
+                        grade = "B"
 
-                report = f"""
-                PROMPTSHIELD SECURITY REPORT
+                    elif efficiency_score >= 50:
+                        grade = "C"
 
-                Prompt:
-                {user_prompt}
+                    else:
+                        grade = "Needs Improvement"
 
-                Threat Type:
-                {result['llm_threat_type']}
+                    # Donut Chart
+                    chart = create_efficiency_chart(
+                        efficiency_score
+                    )
 
-                Risk Score:
-                {result['final_risk_score']}
+                    st.plotly_chart(
+                        chart,
+                        use_container_width=True
+                    )
 
-                Severity:
-                {result['final_severity']}
+                    st.metric(
+                        "Prompt Grade",
+                        grade
+                    )
 
-                Explanation:
-                {result['explanation']}
+                    # Metrics
+                    col1, col2, col3 = st.columns(3)
 
-                Safe Rewrite:
-                {result['safe_rewrite']}"""
+                    with col1:
+                        st.metric(
+                            "Clarity",
+                            optimization_result["clarity"]
+                        )
+
+                    with col2:
+                        st.metric(
+                            "Specificity",
+                            optimization_result["specificity"]
+                        )
+
+                    with col3:
+                        st.metric(
+                            "Structure",
+                            optimization_result["structure"]
+                        )
+
+                    st.markdown("---")
+
+                    # Suggestions
+                    st.subheader("Prompt Improvement Suggestions")
+
+                    for suggestion in optimization_result["suggestions"]:
+
+                        st.info(suggestion)
+
+                    st.markdown("---")
+
+                    # Optimized Prompt
+                    st.subheader("Optimized Prompt")
+
+                    st.success(
+                        optimization_result["optimized_prompt"]
+                    )
                 
-                st.download_button(
-                    label="Download Security Report",
-                    data=report,
-                    file_name="promptshield_report.txt",
-                    mime="text/plain"
-                )
+                else:
+                    st.error("⚠️ Threat Detected")
+
+                    col1, col2 = st.columns(2)
+
+                    with col1:
+                        st.metric("Risk Score", result["final_risk_score"], delta=None)
+
+                    with col2:
+                        st.metric("Severity", result["final_severity"], delta=None)
+                
+
+                    st.markdown("---")
+
+                    st.subheader("Detected Threat Types")
+
+                    st.warning(result["llm_threat_type"])
+                            
+                    st.subheader("Detected Threat Categories")
+
+                    if result["detected_threats"]:
+                        for threat in result["detected_threats"]:
+                            st.error(threat)
+
+                    else:
+                        st.info("No rule-based threats detected.")
+
+                    st.markdown("---")
+
+                    if result["llm_threat_type"] == "Gemini Overloaded":
+
+                        st.warning(
+                            "⚠️ Gemini API is currently overloaded. "
+                            "Using fallback security analysis."
+                        )
+
+                    st.subheader("Threat Explanation")
+
+                    st.write(result["explanation"])
+
+                    st.markdown("---")
+
+                    st.subheader("Safe Rewrite Suggestion")
+                    st.info(result["safe_rewrite"])
+
+                    st.markdown("---")
+
+                    report = f"""
+                    PROMPTSHIELD SECURITY REPORT
+
+                    Prompt:
+                    {user_prompt}
+
+                    Threat Type:
+                    {result['llm_threat_type']}
+
+                    Risk Score:
+                    {result['final_risk_score']}
+
+                    Severity:
+                    {result['final_severity']}
+
+                    Explanation:
+                    {result['explanation']}
+
+                    Safe Rewrite:
+                    {result['safe_rewrite']}"""
+                    
+                    st.download_button(
+                        label="Download Security Report",
+                        data=report,
+                        file_name="promptshield_report.txt",
+                        mime="text/plain"
+                    )
+        except Exception:
+
+            st.error("Analysis temporarily unavailable."
+                     "Please try again"
+            )
 
 elif selected == "Analytics Dashboard":
 
